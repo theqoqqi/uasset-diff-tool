10461da00cce8fc50e49b4f4faf4760949518270
Sazykin Georgii <sazykin.georgii@gmail.com>
2025-05-17 11:07:54 +0300

    refactor: move output logic to new DiffPrinter class

---
+++ UassetComparisonTool/DiffPrinter.cs
@@ -0,0 +1,99 @@
+﻿using UassetComparisonTool.Diffs;
+
+namespace UassetComparisonTool;
+
+public class DiffPrinter {
+
+    private TextWriter Writer;
+
+    public DiffPrinter(TextWriter writer) {
+        Writer = writer;
+    }
+
+    public void PrintDiff(AssetDiff assetDiff) {
+        PrintDiffType(assetDiff, "Asset", 0);
+
+        if (assetDiff.ChangedProperties.Any()) {
+            Writer.WriteLine("  Property changes:");
+
+            foreach (var pd in assetDiff.ChangedProperties) {
+                PrintPropertyDiff(pd, 2);
+            }
+        }
+
+        if (assetDiff.ChangedFunctions.Any()) {
+            Writer.WriteLine("  Function changes:");
+
+            foreach (var fd in assetDiff.ChangedFunctions) {
+                PrintFunctionDiff(fd, 2);
+            }
+        }
+    }
+
+    private void PrintPropertyDiff(PropertyDiff diff, int indent) {
+        PrintDiffType(diff, "Property", indent);
+
+        if (diff.DiffType == DiffType.Changed) {
+            
+            PrintValueChange(diff.Type, "Type", indent + 2);
+            PrintValueChange(diff.StructClass, "StructClass", indent + 2);
+            PrintValueChange(diff.PropertyClass, "PropertyClass", indent + 2);
+            PrintValueChange(diff.ArrayDim, "ArrayDim", indent + 2);
+
+            foreach (var inner in diff.InnerProperties.Values) {
+                PrintPropertyDiff(inner, indent + 1);
+            }
+        }
+    }
+
+    private void PrintFunctionDiff(FunctionDiff diff, int indent) {
+        var prefix = Indent(indent);
+
+        PrintDiffType(diff, "Function", indent);
+
+        if (diff.DiffType == DiffType.Changed) {
+            if (diff.ChangedInputProperties.Any()) {
+                Writer.WriteLine($"{prefix}    Input param changes:");
+
+                foreach (var pd in diff.ChangedInputProperties) {
+                    PrintPropertyDiff(pd, indent + 3);
+                }
+            }
+
+            if (diff.ChangedOutputProperties.Any()) {
+                Writer.WriteLine($"{prefix}    Output param changes:");
+
+                foreach (var pd in diff.ChangedOutputProperties) {
+                    PrintPropertyDiff(pd, indent + 3);
+                }
+            }
+        }
+    }
+
+    private void PrintValueChange<T>(ValueChange<T> change, string title, int indentLevel) {
+        if (change.DiffType == DiffType.Unchanged) {
+            return;
+        }
+        
+        Writer.WriteLine($"{Indent(indentLevel)}{title}: {change.From} => {change.To}");
+    }
+
+    private void PrintDiffType(Diff diff, string title, int indentLevel) {
+        if (diff.DiffType == DiffType.Unchanged) {
+            return;
+        }
+
+        var signs = new Dictionary<DiffType, string> {
+                { DiffType.Added, "+" },
+                { DiffType.Removed, "-" },
+                { DiffType.Changed, "~" },
+        };
+        var sign = signs[diff.DiffType];
+            
+        Writer.WriteLine($"{Indent(indentLevel)}{sign} {title} '{diff.Name}' {diff.DiffType}");
+    }
+
+    private static string Indent(int level) {
+        return new string(' ', level * 2);
+    }
+}
--- UassetComparisonTool/Program.cs
+++ UassetComparisonTool/Program.cs
@@ -14,21 +14,23 @@ internal static class Program {
 
         var pathA = args[0];
         var pathB = args[1];
+        var writer = Console.Out;
 
         if (Directory.Exists(pathA) && Directory.Exists(pathB)) {
-            CompareDirectories(pathA, pathB);
+            CompareDirectories(writer, pathA, pathB);
             return;
         }
 
         if (File.Exists(pathA) && File.Exists(pathB)) {
-            CompareFiles(pathA, pathB);
+            CompareFiles(writer, pathA, pathB);
             return;
         }
 
         Console.WriteLine("Both arguments must be either existing files or existing directories.");
     }
 
-    private static void CompareFiles(string fileA, string fileB) {
+    private static void CompareFiles(TextWriter writer, string fileA, string fileB) {
+        var diffPrinter = new DiffPrinter(writer);
         var assetA = new UAsset(fileA, EngineVersion.VER_UE4_27);
         var assetB = new UAsset(fileB, EngineVersion.VER_UE4_27);
         var context = DiffContext.From(assetA, assetB);
@@ -37,10 +39,11 @@ internal static class Program {
         var assetName = nameA == nameB ? nameA : $"{nameA} => {nameB}";
         var assetDiff = AssetDiff.Create(context, assetName, assetA, assetB);
 
-        PrintAssetDiff(assetDiff);
+        diffPrinter.PrintDiff(assetDiff);
     }
 
-    private static void CompareDirectories(string dirA, string dirB) {
+    private static void CompareDirectories(TextWriter writer, string dirA, string dirB) {
+        var diffPrinter = new DiffPrinter(writer);
         var filesA = GetUassetPaths(dirA);
         var filesB = GetUassetPaths(dirB);
 
@@ -52,7 +55,7 @@ internal static class Program {
             var context = DiffContext.From(assetA, assetB);
             var assetDiff = AssetDiff.Create(context, relPath, assetA, assetB);
 
-            PrintAssetDiff(assetDiff);
+            diffPrinter.PrintDiff(assetDiff);
         }
     }
 
@@ -65,92 +68,5 @@ internal static class Program {
     private static Dictionary<string, string> GetUassetPaths(string directory) {
         return Directory.GetFiles(directory, "*.uasset", SearchOption.AllDirectories)
                 .ToDictionary(fullPath => Path.GetRelativePath(directory, fullPath));
-    }
-
-    private static void PrintAssetDiff(AssetDiff assetDiff) {
-        PrintDiffType(assetDiff, "Asset", 0);
-
-        if (assetDiff.ChangedProperties.Any()) {
-            Console.WriteLine("  Property changes:");
-
-            foreach (var pd in assetDiff.ChangedProperties) {
-                PrintPropertyDiff(pd, 2);
-            }
-        }
-
-        if (assetDiff.ChangedFunctions.Any()) {
-            Console.WriteLine("  Function changes:");
-
-            foreach (var fd in assetDiff.ChangedFunctions) {
-                PrintFunctionDiff(fd, 2);
-            }
-        }
-    }
-
-    private static void PrintPropertyDiff(PropertyDiff diff, int indent) {
-        PrintDiffType(diff, "Property", indent);
-
-        if (diff.DiffType == DiffType.Changed) {
-            
-            PrintValueChange(diff.Type, "Type", indent + 2);
-            PrintValueChange(diff.StructClass, "StructClass", indent + 2);
-            PrintValueChange(diff.PropertyClass, "PropertyClass", indent + 2);
-            PrintValueChange(diff.ArrayDim, "ArrayDim", indent + 2);
-
-            foreach (var inner in diff.InnerProperties.Values) {
-                PrintPropertyDiff(inner, indent + 1);
-            }
-        }
-    }
-
-    private static void PrintFunctionDiff(FunctionDiff diff, int indent) {
-        var prefix = Indent(indent);
-
-        PrintDiffType(diff, "Function", indent);
-
-        if (diff.DiffType == DiffType.Changed) {
-            if (diff.ChangedInputProperties.Any()) {
-                Console.WriteLine($"{prefix}    Input param changes:");
-
-                foreach (var pd in diff.ChangedInputProperties) {
-                    PrintPropertyDiff(pd, indent + 3);
-                }
-            }
-
-            if (diff.ChangedOutputProperties.Any()) {
-                Console.WriteLine($"{prefix}    Output param changes:");
-
-                foreach (var pd in diff.ChangedOutputProperties) {
-                    PrintPropertyDiff(pd, indent + 3);
-                }
-            }
-        }
-    }
-
-    private static void PrintValueChange<T>(ValueChange<T> change, string title, int indentLevel) {
-        if (change.DiffType == DiffType.Unchanged) {
-            return;
-        }
-        
-        Console.WriteLine($"{Indent(indentLevel)}{title}: {change.From} => {change.To}");
-    }
-
-    private static void PrintDiffType(Diff diff, string title, int indentLevel) {
-        if (diff.DiffType == DiffType.Unchanged) {
-            return;
-        }
-
-        var signs = new Dictionary<DiffType, string> {
-                { DiffType.Added, "+" },
-                { DiffType.Removed, "-" },
-                { DiffType.Changed, "~" },
-        };
-        var sign = signs[diff.DiffType];
-            
-        Console.WriteLine($"{Indent(indentLevel)}{sign} {title} '{diff.Name}' {diff.DiffType}");
-    }
-
-    private static string Indent(int level) {
-        return new string(' ', level * 2);
     }
 }
