ba25d21121d7e7748ed801b45cbd8fbf26307c50
Sazykin Georgii <sazykin.georgii@gmail.com>
2025-05-19 12:01:16 +0300

    refactor: move OptionExtensions class to separate file

---
+++ UAssetDiffTool/OptionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.CommandLine;
+
+namespace UAssetDiffTool;
+
+internal static class OptionExtensions {
+    public static Option<string?> ExistingOrCreateableFile(this Option<string?> opt) {
+        opt.AddValidator(result => {
+            var value = result.GetValueOrDefault<string?>();
+
+            if (string.IsNullOrEmpty(value)) {
+                return;
+            }
+
+            try {
+                var dir = Path.GetDirectoryName(value);
+
+                if (!string.IsNullOrEmpty(dir) && !Directory.Exists(dir)) {
+                    Directory.CreateDirectory(dir);
+                }
+            }
+            catch {
+                result.ErrorMessage = $"Invalid path: {value}";
+            }
+        });
+
+        return opt;
+    }
+}
--- UAssetDiffTool/Program.cs
+++ UAssetDiffTool/Program.cs
@@ -248,28 +248,3 @@ internal static class Program {
         return Path.ChangeExtension(shortPath, null);
     }
 }
-
-internal static class OptionExtensions {
-    public static Option<string?> ExistingOrCreateableFile(this Option<string?> opt) {
-        opt.AddValidator(result => {
-            var value = result.GetValueOrDefault<string?>();
-
-            if (string.IsNullOrEmpty(value)) {
-                return;
-            }
-
-            try {
-                var dir = Path.GetDirectoryName(value);
-
-                if (!string.IsNullOrEmpty(dir) && !Directory.Exists(dir)) {
-                    Directory.CreateDirectory(dir);
-                }
-            }
-            catch {
-                result.ErrorMessage = $"Invalid path: {value}";
-            }
-        });
-
-        return opt;
-    }
-}
