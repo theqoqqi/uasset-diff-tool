daaf7e3aefb611aa4ff20ca130312d1d16a9fc75
Sazykin Georgii <sazykin.georgii@gmail.com>
2025-05-20 12:26:17 +0300

    feat: add template web-diff-viewer project

---
+++ web-diff-viewer/src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+
+function App() {
+    return (
+        <div className={styles.App}>
+            <h1>Web Diff Viewer</h1>
+        </div>
+    );
+}
+
+export default App;
