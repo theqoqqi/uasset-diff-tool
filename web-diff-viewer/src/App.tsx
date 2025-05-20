4f4ee47593e29aae5f9f878a78c178a1f2f5eacb
Sazykin Georgii <sazykin.georgii@gmail.com>
2025-05-20 18:16:52 +0300

    feat: add single file diff support

--- web-diff-viewer/src/App.tsx
+++ web-diff-viewer/src/App.tsx
@@ -5,15 +5,22 @@ import Sidebar from './components/Sidebar';
 import DetailsView from './components/details/DetailsView';
 
 export default function App() {
-    const [data, setData] = useState<DiffJson | null>(null);
+    const [data, setData] = useState<DiffJson | AssetDiff | null>(null);
     const [selected, setSelected] = useState<AssetDiff | null>(null);
+    const isSingleAsset = data && 'Name' in (data as AssetDiff);
+    const isMultipleAssets = data && !isSingleAsset;
 
     return (
         <>
             {!data && <UploadArea onLoad={setData} />}
-            {data && (
+            {isSingleAsset && (
                 <div style={{ display: 'flex', height: '100vh' }}>
-                    <Sidebar data={data} onSelect={setSelected} />
+                    <DetailsView asset={data as AssetDiff} />
+                </div>
+            )}
+            {isMultipleAssets && (
+                <div style={{ display: 'flex', height: '100vh' }}>
+                    <Sidebar data={data as DiffJson} onSelect={setSelected} />
                     <DetailsView asset={selected} />
                 </div>
             )}
